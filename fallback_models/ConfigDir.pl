6a7556702d4604b26a2d8d1af2f6c9538bfee1b9
Dominique Dumont <dod@debian.org>
2015-04-28 19:55:50 +0200

    updated fallback_model to fix test failure on BSD

--- fallback_models/ConfigDir.pl
+++ fallback_models/ConfigDir.pl
@@ -1,12 +1,21 @@
 # Generated file. Do not edit
 
 [
-          [
-            'name',
-            'Xorg::ConfigDir',
-            'generated_by',
-            'Config::Model Build.PL',
-            'config_dir',
-            '/etc/X11'
-          ]
-        ]
+  [
+    'name',
+    'Xorg::ConfigDir',
+    'class_description',
+    'Class used to tune location of xorg.conf which is not always in /etc/X11.',
+    'generated_by',
+    'Config::Model Build.PL',
+    'read_config',
+    [
+      {
+        'auto_create' => 1,
+        'backend' => 'Xorg',
+        'config_dir' => '/etc/X11',
+        'file' => 'xorg.conf'
+      }
+    ]
+  ]
+]
